0ce8eb98ed90277d7d9b835d5026560055eabdbf
danielerhabor <erhabord@yahoo.com>
2017-12-10 23:09:03 -0500

    Finishing up changes to DownloadView and ImageService. Had to handle multiple tags and implement function to use entry.toURL()

--- www/js/ImageService.js
+++ www/js/ImageService.js
@@ -1,13 +1,15 @@
 var ImageService = function() {
-
+    var storage = window.localStorage; // initalize local storage object
+    var fname_URL = []; // initialize URL for array of {"fileName": entry.toURL()} dictionary
     this.initialize = function() {
         // No Initialization required
         var deferred = $.Deferred();
+
         deferred.resolve();
         return deferred.promise();
     }
 
-    this.downloadImage = function(imageURL, imageTag) {
+    this.downloadImage = function(imageURL, imageTags) {
 
         var deferred = $.Deferred();
 
@@ -23,13 +25,43 @@ var ImageService = function() {
         var fileName = my_url_split.[my_url_split.length - 1];      // Get the last element in the array 
                                                                     // if the url was "http://s14.postimg.org/i8qvaxyup/bitcoin1.jpg" then the fileName would be 
                                                                     // bitcoin1.jpg
+        var my_tags_array = imageTags.split(" ").filter(i => i!=="");      // And array of tags namely ["tag1", "tag2", "tag3"]
+                                                                                 // Splits on single space,
+                                                                                 //  removes empty strings in array after split
+
         
+        var fileURL = cordova.file.cacheDirectory + fileName;     // Here fileURL is where we will save the file "/data/data/com.daniel.workshop/cache/bitcoin1.jpg"
+        fileTransfer.download(
+            uri,            // source
+            fileURL,        // target; where the file will be stored
+            function(entry) {
+                alert("Download Successful!");          // Do this when the download is successful
+                console.log("download complete: " + entry.toURL());     // entry.toURL() helps us get a URL for the image that we can use with href or src
 
 
+                for (var i = 0; i < my_tags_array.length; i++){
+                     storage.setItem(my_tags_array[i], fileName);     // store in localStorage tuples of (tag, fileName) 
+                                                                      // Multiple tags can have the same fileName
+                }
 
-        
-    }
+                fname_URL.push( {fileName: entry.toURL()} );      // append a dictionary of the fileName : entry.toURL to our array fname_URL usefule for src with img;
+                
 
+
+            },
+            function(error) {
+                alert("Download Failed!");          // Do this when the download fails
+                console.log("download error source " + error.source);
+                console.log("download error target " + error.target);
+                console.log("download error code " + error.code);
+            },
+            false,
+            {
+                headers: {
+                    "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                }
+            }
+        );
 
 
 
